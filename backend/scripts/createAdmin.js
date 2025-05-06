64da5627839bb1b351c051351c27c98331596c8c
Mallik vinukonda <158685334+Mallik-vinukonda@users.noreply.github.com>
2025-05-06 19:12:04 +0530

    Update createAdmin.js

--- backend/scripts/createAdmin.js
+++ backend/scripts/createAdmin.js
@@ -8,8 +8,8 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/yourdbname
 async function createAdmin() {
   await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-  const email = 'mallikvinukonda123@gmail.com'; // CHANGE THIS
-  const password = '123456789'; // CHANGE THIS
+  const email = 'mallikvinukonda123@gmail.com'; 
+  const password = '123456789'; 
   const name = 'SuperAdmin';
 
   const hashedPassword = await bcrypt.hash(password, 12);
